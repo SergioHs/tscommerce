d25b9e289aaa432f4962e0005fbe1bdc3a30956e
SergioHs <shs_28@hotmail.com>
2023-11-22 20:41:14 -0300

    userModel e userController fix

--- src/data/database.ts
+++ src/data/database.ts
@@ -5,6 +5,8 @@ const sequelize = new Sequelize('mysql://root:root@localhost:3306/mystore');
 
 if(sequelize){
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
+} else {
+    console.error('Não foi possível conectar ao banco de dados!');
 }
 
 export default sequelize;
