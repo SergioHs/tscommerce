d93f1afbd184a9884edce17245e2c952442be8a8
SergioHs <shs_28@hotmail.com>
2023-11-23 21:36:08 -0300

    Migrations fix

--- migrations/20231123235956-create-cart-product.js
+++ migrations/20231123235956-create-cart-product.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('CartProducts', {
+    await queryInterface.createTable('cart_products', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- seeders/20231124001252-demo-cart-products.js
+++ seeders/20231124001252-demo-cart-products.js
@@ -4,9 +4,9 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('cart_products', [
-      { cart_products_quatity: 2, cart_id: 1, product_id: 1 },
-      { cart_products_quatity: 3, cart_id: 1, product_id: 2 },
-      { cart_products_quatity: 1, cart_id: 2, product_id: 3 },
+      { cart_products_quantity: 2, cart_id: 1, product_id: 1 },
+      { cart_products_quantity: 3, cart_id: 1, product_id: 2 },
+      { cart_products_quantity: 1, cart_id: 2, product_id: 3 },
     ], {});
   },
 
